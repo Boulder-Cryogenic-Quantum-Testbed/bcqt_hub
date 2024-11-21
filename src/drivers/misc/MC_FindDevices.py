3ae7a4b987dd5d2bfd955bdffe3daf235a67bb3b
jorger99 <jorge.ramirez@colorado.edu>
2024-11-20 21:35:44 -0700

    changed MC_FindDevices so that it isn't run on importing bcqt_hub

--- src/drivers/misc/MC_FindDevices.py
+++ src/drivers/misc/MC_FindDevices.py
@@ -1,44 +1,46 @@
 import socket
 
-# broadcast address is the bitwise OR between IP and bit-complement of the subnet mask
-addr_SND = ('192.168.0.255', 4950)  # broadcast address / MCL test equipment listens on port 4950
-addr_RCV = ('', 4951)               # MCL test equipment replies on port 4951
- 
-UDPSock_SND = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # Create socket
-UDPSock_RCV = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # Create socket
+if __name__ is "__main__":
 
-UDPSock_RCV.bind(addr_RCV)
-UDPSock_RCV.settimeout(1)
-Data_RCV=""
-
-all_snds = [str.encode("MCLRFSWITCH?"), str.encode("MCLDAT?")]    # Query for the relevant product family (encoded to bytes)
-
-num_devices = 3
-
-for Data_SND in all_snds:
-    print("Sending message '{Data_SND}'")
-    UDPSock_SND.sendto(Data_SND, addr_SND)
-
-    print(f"    Listening for up to {num_devices} devices...")
+    # broadcast address is the bitwise OR between IP and bit-complement of the subnet mask
+    addr_SND = ('192.168.0.255', 4950)  # broadcast address / MCL test equipment listens on port 4950
+    addr_RCV = ('', 4951)               # MCL test equipment replies on port 4951
     
-    i=0
-    while i<num_devices:                          # Search for up to x units
+    UDPSock_SND = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # Create socket
+    UDPSock_RCV = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # Create socket
 
-        print(f"    Device {i+1}")
+    UDPSock_RCV.bind(addr_RCV)
+    UDPSock_RCV.settimeout(1)
+    Data_RCV=""
+
+    all_snds = [str.encode("MCLRFSWITCH?"), str.encode("MCLDAT?")]    # Query for the relevant product family (encoded to bytes)
+
+    num_devices = 3
+
+    for Data_SND in all_snds:
+        print("Sending message '{Data_SND}'")
+        UDPSock_SND.sendto(Data_SND, addr_SND)
+
+        print(f"    Listening for up to {num_devices} devices...")
         
-        try:
-            Data_RCV, addr_RCV = UDPSock_RCV.recvfrom(4951)
-            print(str(Data_RCV).replace('\\r\\n','\n        ').replace("b'Model Name", "        Model Name"))
+        i=0
+        while i<num_devices:                          # Search for up to x units
 
-        except:                         # Timeout error if no more responses
-            print("    No data received.")
+            print(f"    Device {i+1}")
             
-        i=i+1
+            try:
+                Data_RCV, addr_RCV = UDPSock_RCV.recvfrom(4951)
+                print(str(Data_RCV).replace('\\r\\n','\n        ').replace("b'Model Name", "        Model Name"))
 
-print("End of UDP listening...")
+            except:                         # Timeout error if no more responses
+                print("    No data received.")
+                
+            i=i+1
 
-print('Client stopped.')
+    print("End of UDP listening...")
+
+    print('Client stopped.')
 
 
-UDPSock_SND.close()             # Close sockets
-UDPSock_RCV.close()
+    UDPSock_SND.close()             # Close sockets
+    UDPSock_RCV.close()
