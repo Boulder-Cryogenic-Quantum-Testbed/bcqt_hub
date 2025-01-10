89b0489e348afb1196922802339a59a137e79945
jorger99 <jorge.ramirez@colorado.edu>
2025-01-10 16:17:56 -0700

    added ability to plot two csvs together

--- experiments/VNA Experiments/VNA Characterization/TakeMeasurements.py
+++ experiments/VNA Experiments/VNA Characterization/TakeMeasurements.py
@@ -107,11 +107,66 @@ meas_name = "Line4_TonyTa_NbSi_03"
 
 filename, filepath = qh.Archive_Data(PNA_X, s2p_df, meas_name=meas_name, expt_category=expt_category, all_axes=all_axes)
 
+# %%
 
-# %% manually load data
+basepath = Path(r"C:\Users\Lehnert Lab\GitHub\bcqt_hub\experiments\VNA Experiments\VNA Characterization\data\HEMT_Testing")
+# file_dir = basepath / r"Line26_InsertionLoss"
+file_dir = basepath / r"NbTi_Cable"
 
-load_filepath = r"C:\Users\Lehnert Lab\GitHub\bcqt-ctrl\prototype_msmt_code\experiments\VNA Experiments\VNA Characterization\data_jin_characterization\S12_MXC\001_Input_Line24_to_Attenuators\Input_Line24_to_Attenuators_001.csv"
+# %% manually load data and plot
 
-loaded_df, all_magns, all_phases, freqs = qh.Load_CSV(load_filepath)
+filepath = list(file_dir.glob("*.csv"))[-1]
 
-all_axes = qh.plot_s2p_df(loaded_df, plot_complex=True, track_min=False, title=meas_name, do_edelay_fit=False)
+loaded_df, all_magns, all_phases, freqs = qh.load_csv(filepath)
+loaded_df.drop(["datetime.now()"], inplace=True)
+
+
+all_axes = qh.plot_s2p_df(loaded_df, plot_complex=False, track_min=False, title=filepath.stem, do_edelay_fit=False)
+
+
+# %% two files and two dfs
+file_dir_1 =  basepath / r"HEMT_ON_-55dBm_Line25"
+file_dir_2 =  basepath / r"HEMT_ON_-55dBm_Line27"
+# file_dir_2 =  basepath / r"HEMT_OFF_-55dBm_Line27_Background"
+assert file_dir_1.exists() and file_dir_2.exists()
+
+filepath_1 = list(file_dir_1.glob("*.csv"))[-1]
+filepath_2 = list(file_dir_2.glob("*.csv"))[-1]
+
+
+loaded_df_1, all_magns_1, all_phases_1, freqs_1 = qh.load_csv(filepath_1)
+loaded_df_2, all_magns_2, all_phases_2, freqs_2 = qh.load_csv(filepath_2)
+
+freq = loaded_df_2["Frequency"].values[1:].astype(float)
+magn_1 = loaded_df_1["S21 magn_dB"].values[1:].astype(float)
+magn_2 = loaded_df_2["S21 magn_dB"].values[1:].astype(float)
+
+# %%
+
+mosaic = "AA\nAA\nBB"
+fig1, axes = plt.subplot_mosaic(mosaic, figsize=(8, 8))
+ax1, ax2 = axes["A"], axes["B"]
+
+ax1.set_title("VNA Power = -55 dBm")
+ax1.plot(freq, magn_1, label="HEMT 25")
+ax1.plot(freq, magn_2, label="HEMT 27")
+ax1.legend()
+
+ax2.set_title("[HEMT 27 - HEMT 25]")
+ax2.plot(freq, (magn_1 - magn_2))
+ax2.axhline(0, linestyle=':', color='k')
+
+
+for ax in (ax1, ax2):
+    ax.set_xlim([4e9, 8e9])
+    
+
+
+fig1.suptitle("Optimized HEMT S21 Measurements")
+    
+ax1.set_ylim([0, 30])
+ax2.set_ylim([-20, 10])
+
+fig1.tight_layout()
+
+# %%
