d32cbd6aaea01324222cb3b3b992c9a8bb7becb2
Alan La <jellyboxmeow@gmail.com>
2025-02-06 14:29:42 -0700

    added skeleton code in datahandler to load metadata

--- bcqt_hub/src/DataHandler.py
+++ bcqt_hub/src/DataHandler.py
@@ -1,7 +1,8 @@
 # %%
 import numpy as np
 import pandas as pd
-import pathlib as Path
+import json
+from pathlib import Path
 from datetime import datetime
 
 
@@ -9,14 +10,9 @@ class DataSet():
     """
         this class compartmentalizes just one set of data
         
-        the data will be saved in pandas dataframes in self.data
-        
-        the scope of the methods include:
-            - initialization requires a dataset, an experiment name, and units
-            - methods for handling different input data types (dict, list, np.array, dataframe)
-            - stores metadata such as units and experimental configs
-            - generates relevant metadata such as timestamps, filepaths
-       
+        One Dataset should correspond to one measurement
+
+        Basically the DataSet is the book and the DataHandler is the bookshelf
     """
     
     # def __init__(self, data, expt_name:str, meas_label:str, units:str, **kwargs):
@@ -54,22 +50,22 @@ class DataSet():
     
     #### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
     
-    def append_df(self, data_df, axis=1):
-        """ if a DataFrame is given, concat with existing """
-        self.data = pd.concat([self.data, data_df], axis=axis)
-        self.data.reset_index(drop=True, inplace=True)
+    # def append_df(self, data_df, axis=1):
+    #     """ if a DataFrame is given, concat with existing """
+    #     self.data = pd.concat([self.data, data_df], axis=axis)
+    #     self.data.reset_index(drop=True, inplace=True)
         
-    def append_row_df(self, data_row):
-        self.append_df(data_row, axis=0)
+    # def append_row_df(self, data_row):
+    #     self.append_df(data_row, axis=0)
         
-    def append_dict(self, data_dict):
-        """ if a dict is given, append to dataframe as a column or row """
-        data_df = pd.DataFrame.from_dict(data_dict)
-        self.append_df(data_df)
+    # def append_dict(self, data_dict):
+    #     """ if a dict is given, append to dataframe as a column or row """
+    #     data_df = pd.DataFrame.from_dict(data_dict)
+    #     self.append_df(data_df)
             
-    def append_array(self, data, label):
-        """ if an array is given, require a label and append to dataframe  """
-        self.append_dict( {label : data} )
+    # def append_array(self, data, label):
+    #     """ if an array is given, require a label and append to dataframe  """
+    #     self.append_dict( {label : data} )
 
     def load_csv(self, csv_path_string):
         return pd.read_csv(csv_path_string, index_col=0)
@@ -164,8 +160,8 @@ class DataHandler(dict):
             self.load_dataset(file)
     
     # Load a singular dataset from a given path object
-    def load_dataset(self, file_path):
-        if isinstance(file_path, Path.PurePath) is False or isinstance(file_path, Path.Path) is False :
+    def load_dataset(self, file_path: Path):
+        if isinstance(file_path, Path) is False :
             raise TypeError("argument 'file_path' is not a Path object.")
         
         dset = self.create_dataset(file_path)
@@ -184,42 +180,11 @@ class DataHandler(dict):
                 # print(value.head(2))
             display(value.head(2))
 
-    def load_metadata(self):
-        
+    # Come back to do to insert a json file (in skele form)
+    def load_metadata_from_file(self, json_path: Path):
+        with open(json_path, 'r') as json_file:
+            json_data = json.load(json_file)
+
+        # print(json_data)
+        display(json_data)
         pass
-
-
-#%%
-
-# %%
-from pathlib import Path
-import sys
-
-current_dir = Path(".")
-parent_dir = Path("..")
-grandparent_dir = Path("../..")
-github_dir = Path("C:/Users/jelly/bcqt_hub")
-
-script_filename = Path(__file__).stem
-
-# import bcqt_hub.experiments.quick_helpers as qh
-sys.path.append(github_dir)
-
-data_dir = grandparent_dir / "experiments" / "TWPA Calibration" /r"data"/ "cooldown59"
-
-print(data_dir)
-
-# use glob to grab all files
-all_measurements = list(data_dir.glob("Line1*/*TWPA*"))
-print(all_measurements)
-
-# for measurement in all_measurements:
-
-meas_idx = 1
-measurement = all_measurements[meas_idx]
-
-print(f"Measurement [{meas_idx}/{len(all_measurements)}]:  {measurement.name}")
-all_traces = sorted(list(measurement.glob("*TWPA*")))
-
-packaged_data = []
-# %%
