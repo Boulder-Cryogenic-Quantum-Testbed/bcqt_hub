abab567288fd036e8041333d1510e520abf664a6
Alan La <jellyboxmeow@gmail.com>
2025-02-13 12:40:53 -0700

    metadata changes, need to fix in load_dir

--- bcqt_hub/src/DataHandler.py
+++ bcqt_hub/src/DataHandler.py
@@ -40,12 +40,13 @@ class DataSet():
         
     #     if "configs" in kwargs:
     #         self.add_configs(kwargs["configs"])
-    def __init__(self, csv_path):
+    def __init__(self, csv_path, metadata_dict):
         """
 
         """
         self.data = self.load_csv(csv_path)
         self.file_name = csv_path
+        self.metadata = metadata_dict
         
     
     #### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
@@ -76,43 +77,33 @@ class DataSet():
     def get_file_name(self):
         return str(self.file_name.stem)
     
+    def get_meta_data(self):
+        return self.metadata
+    
     #### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
-    
-    
-    def generate_metadata(self):
-        pass
-    
-    def display_metadata(self, print_output=False):
-        data, metadata = self.data, self.metadata
-        str_repr = f"label: {metadata["label"]}\n" + " "*11 + \
-                   f"len(data) = {len(data)}\n" + " "*11 + \
-                   f"units = {metadata["units"]}\n" + " "*11
+
+    # Make metadata editor if needed
+
+    # def display_metadata(self, print_output=False):
+    #     data, metadata = self.data, self.metadata
+    #     str_repr = f"label: {metadata["label"]}\n" + " "*11 + \
+    #                f"len(data) = {len(data)}\n" + " "*11 + \
+    #                f"units = {metadata["units"]}\n" + " "*11
         
-        if print_output is True:
-            print(str_repr)
-        else:
-            return str_repr
+    #     if print_output is True:
+    #         print(str_repr)
+    #     else:
+    #         return str_repr
     
-    def append_to_metadata(self, **kwargs):
+    # def append_to_metadata(self, **kwargs):
     
-        for key, val in kwargs.items():
-            if key in self.metadata:
-                self.print_datahandler(f"        [DataSet] Overwriting {key}={self.metadata[key]} with {key}={val}")
-            else:
-                print(f"Saving {key}={val}")
+    #     for key, val in kwargs.items():
+    #         if key in self.metadata:
+    #             self.print_datahandler(f"        [DataSet] Overwriting {key}={self.metadata[key]} with {key}={val}")
+    #         else:
+    #             print(f"Saving {key}={val}")
     
-            self.metadata[key] = val
-            
-            
-    #### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
-        
-        
-    def add_configs(self, configs:dict):
-        self.configs = configs
-    
-    def update_configs(self, new_configs):
-        self.configs.update(new_configs)
-        
+    #         self.metadata[key] = val
             
     #### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
             
@@ -151,40 +142,74 @@ class DataHandler(dict):
         self.list_of_datasets = {}
 
     # Load a directory of path objects and create a corresponding dataset object for all of them
-    def load_data_directory(self, path):
+    def load_data_directory(self, path:Path, mdict:dict):
         if path.is_dir() is False:
             raise TypeError("argument 'path' is not a directory object.")
 
         data_dir_files = list(path.glob("*"))
+        self.create_metadata_for_directory(path, mdict)
         for file in data_dir_files:
-            self.load_dataset(file)
+            self.load_dataset(file, mdict)
     
     # Load a singular dataset from a given path object
-    def load_dataset(self, file_path: Path):
+    def load_dataset(self, file_path: Path, metadict:dict):
         if isinstance(file_path, Path) is False :
             raise TypeError("argument 'file_path' is not a Path object.")
         
-        dset = self.create_dataset(file_path)
+        dset = self.create_dataset(file_path, metadict)
         file_name = str(file_path.stem)
         self.list_of_datasets[file_name] = dset
 
     # Create a dataset object from a path object 
-    def create_dataset(self, csv_path):
-        dset = DataSet(csv_path)
-        return dset.get_data()
+    def create_dataset(self, csv_path, metadict):
+        dset = DataSet(csv_path, metadict)
+        return dset
     
-    def display_datasets(self):
+    def display_datasets(self, number_of_rows=2):
         for key, value in self.list_of_datasets.items():
             print(key)
             # Uncomment the print if not using the juypter notebook and comment out the display
-                # print(value.head(2))
-            display(value.head(2))
+                # print(value.head(number_of_rows))
+            display(value.data.head(number_of_rows))
+            # display(value.get_meta_data())
 
+    def create_metadata_for_directory(self, dir_path:Path, mdict:dict):
+        current_json = list(dir_path.glob("*.json"))
+        if len(current_json) == 0:
+            with open("metadata.json", "w", encoding="utf8") as json_file:
+                json.dump(mdict, json_file, indent=4)
+        # print("Json file already exists")
+    
     # Come back to do to insert a json file (in skele form)
-    def load_metadata_from_file(self, json_path: Path):
-        with open(json_path, 'r') as json_file:
-            json_data = json.load(json_file)
+    # do we (2) want to try to store the actual metadata
 
-        # print(json_data)
-        display(json_data)
-        pass
+    # For point 2 could see creating different amoutns of columns for each dataset
+    #   which could be tedious but overall more helpful as it is more accessible
+
+    # How do we want to handle export the datahandler and/or all the datasets?
+
+    # HW: Test the datahandler/dataset on the massive amount of CSV files directory and 
+    #   make sure the datahandler creates all the respective dataframes 
+    #   and creates the corresponding json file for the relevant metadata / directory 
+    #       (make metadata simple like name)
+    #   Make sure in juypter notebook
+
+# %%
+
+# Test Code for metadata
+if __name__ == "__main__":
+    # Initializing
+    dHandler = DataHandler()
+    cur_dir = Path("./")
+    test_csv = list(cur_dir.glob("*.csv"))[0]
+
+    innerdict = {"fish":"yellow"}
+    mdict = {"file_name": str(test_csv.stem), "cat":"joe", 5:"big", "innerdict": innerdict}
+    # dHandler.store_metadata_into_dataset(test_csv, mdict)
+    dHandler.load_dataset(test_csv, mdict)
+    dHandler.display_datasets()
+    dHandler.create_metadata_for_directory(test_csv, mdict)
+    
+
+
+# %%
